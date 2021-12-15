de23d6e95284de0999765c1c7b84fe81722c4e28
Chris Turcios <christopher.d.turcios@gmail.com>
2021-12-15 12:43:27 -0800

    ETL process successfully loads transformed data into the database, but an error occurs stating that javascript heap is out of memory, need to review'

--- database-psql/dataset_converter.js
+++ database-psql/dataset_converter.js
@@ -20,6 +20,8 @@ const parseCSVFiles = async (filePath) => {
     if (filePath === reviewsCSVFilePath) {
       let csvStream = csv.parseFile(reviewsCSVFilePath, { headers: true })
         .on('data', row => {
+
+          // transform data to match database field types
           let id = parseInt(row.id);
           let product_id = parseInt(row.product_id);
           let rating = parseInt(row.rating);
@@ -33,6 +35,7 @@ const parseCSVFiles = async (filePath) => {
           let response = !!row.response && row.response !== 'null' && row.response !== undefined ? row.repsonse : null;
           let helpfulness = parseInt(row.helpfulness);
 
+          // insert data into reviews table
           let query = {
             text: `INSERT INTO reviews(id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
             values: [id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness]
@@ -50,10 +53,13 @@ const parseCSVFiles = async (filePath) => {
     if (filePath === reviewsPhotosCSVFilePath) {
       let csvStream = csv.parseFile(reviewsPhotosCSVFilePath, { headers: true })
         .on('data', row => {
+
+          // transform data to match database field types
           let id = parseInt(row.id);
           let review_id = parseInt(row.review_id);
           let url = row.url;
 
+          // insert data into photos table
           let query = {
             text: 'INSERT INTO photos(id, review_id, url) VALUES($1, $2, $3)',
             values: [id, review_id, url]
@@ -71,10 +77,13 @@ const parseCSVFiles = async (filePath) => {
     if (filePath === characteristicsCSVFilePath) {
       let csvStream = csv.parseFile(characteristicsCSVFilePath, { headers: true })
         .on('data', row => {
+
+          // transform data to match database field types
           let id = parseInt(row.id);
           let product_id = parseInt(row.product_id);
           let name = row.name;
 
+          // insert data into characteristics table
           let query = {
             text: 'INSERT INTO characteristics(id, product_id, name) VALUES($1, $2, $3)',
             values: [id, product_id, name]
@@ -92,11 +101,14 @@ const parseCSVFiles = async (filePath) => {
     if (filePath === characteristic_reviewsCSVFilePath) {
       let csvStream = csv.parseFile(characteristic_reviewsCSVFilePath, { headers: true })
         .on('data', row => {
+
+          // transform data to match database field types
           let id = parseInt(row.id);
           let characteristic_id = parseInt(row.characteristic_id);
           let review_id = parseInt(row.review_id);
           let value = row.value;
 
+          // insert data into characteristic_reviews table
           let query = {
             text: 'INSERT INTO characteristic_reviews(id, characteristic_id, review_id, value) VALUES($1, $2, $3, $4)',
             values: [id, characteristic_id, review_id, value]
