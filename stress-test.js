34237c94e12f981b00e8b88a843fa0a86c98daaa
Chris Turcios <christopher.d.turcios@gmail.com>
2022-01-09 18:46:06 -0800

    Stress test configs updated

--- stress-test.js
+++ stress-test.js
@@ -5,6 +5,8 @@ export const options = {
   stages: [
     { duration: '2m', target: 100 },
     { duration: '5m', target: 100 },
+    { duration: '2m', target: 600 },
+    { duration: '5m', target: 600 },
     { duration: '2m', target: 800 },
     { duration: '5m', target: 800 },
     { duration: '2m', target: 1000 },
@@ -17,7 +19,7 @@ export default function () {
   const BASE_URL = 'http://localhost:3000'; // make sure this is not production
 
   const responses = http.batch([
-    ['GET', `${BASE_URL}/api/reviews/59553`, null, { tags: { name: 'Reviews59553' } }]
+    ['GET', `${BASE_URL}/api/reviews/59553`, null, { tags: { name: 'Reviews' } }]
   ]);
 
   sleep(1);
